ccb3bbb8a60d9420fe607bdcf2a4e907a49bdb48
lenovopc <none>
2015-09-30 15:01:31 +0800

    -

--- HelloWorld/Utils/Helper.cs
+++ HelloWorld/Utils/Helper.cs
@@ -12,7 +12,6 @@ namespace StudyOnline.Utils
     {
         public static MvcHtmlString Uploadify(this HtmlHelper htmlHelper, String selector)
         {
-
             return new MvcHtmlString("");
         }
 
@@ -23,20 +22,16 @@ namespace StudyOnline.Utils
             builder.AppendLine("    <div class='pages'>");
             builder.AppendLine("        <span>显示</span>");
             builder.AppendLine("        <select name=\"numPerPage\" onchange=\"navTabPageBreak({numPerPage:this.value})\">");
-            builder.AppendLine("            <option value='20'>20</option>  ");
-            builder.AppendLine("            <option value='50'>50</option>  ");
-            builder.AppendLine("            <option value='80'>80</option>  ");
-            builder.AppendLine("            <option value='100'>100</option>");
-            builder.AppendLine("            <option value='200'>200</option>");
+            builder.AppendLine(String.Format("            <option value='20'   {0}>20</option>", list.PageSize == 20 ? " selected='selected'" : ""));
+            builder.AppendLine(String.Format("            <option value='50'   {0}>50</option>", list.PageSize == 50 ? " selected='selected'" : ""));
+            builder.AppendLine(String.Format("            <option value='80'   {0}>80</option>", list.PageSize == 80 ? " selected='selected'" : ""));
+            builder.AppendLine(String.Format("            <option value='100' {0}>100</option>", list.PageSize == 100 ? "selected='selected'" : ""));
+            builder.AppendLine(String.Format("            <option value='200' {0}>200</option>", list.PageSize == 200 ? "selected='selected'" : ""));
             builder.AppendLine("        </select>");
             builder.AppendLine(String.Format("        <span>条，共{0}条</span>", list.TotalItemCount));
             builder.AppendLine("    </div>");
-            builder.AppendLine(String.Format("    <div class=\"pagination\" targettype=\"navTab\" totalcount='{0}' numperpage='{1}' pagenumshown='{2}' currentpage='{3}'></div>",
-                list.TotalItemCount, list.PageSize, pagenumshown, list.CurrentPageIndex));
-
+            builder.AppendLine(String.Format("    <div class=\"pagination\" targettype=\"navTab\" totalcount='{0}' numperpage='{1}' pagenumshown='{2}' currentpage='{3}'></div>", list.TotalItemCount, list.PageSize, pagenumshown, list.CurrentPageIndex));
             builder.AppendLine("</div>");
-
-
             return new MvcHtmlString(builder.ToString());
         }
     }
