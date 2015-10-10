dd1259f44df288f56ab899b1b423d4875eb5c611
lenovopc <none>
2015-10-10 17:04:00 +0800

    -

--- HelloWorld/Controllers/NewClassController.cs
+++ HelloWorld/Controllers/NewClassController.cs
@@ -9,6 +9,9 @@ using System.Web.Mvc;
 
 namespace StudyOnline.Controllers
 {
+    /// <summary>
+    /// JSON接口类,所有的列表操作,如果相关Id为空或者小于0,都表示条件为(o=>true)
+    /// </summary>
     public class NewClassController : Controller
     {
         private StudyOnlineEntities db = new StudyOnlineEntities();
@@ -32,21 +35,16 @@ namespace StudyOnline.Controllers
             return Json(data, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult GetDocs(int? folderId, int? levelId, int? skip, int? take)
+        public ActionResult GetDocs(int? folderId, int? skip, int? take)
         {
-            Expression<Func<Document, bool>> predicateFolder = o => true;
-            Expression<Func<Document, bool>> predicateLevel = o => true;
+            Func<Document, bool> predicateFolder = o => true;
 
             if (folderId != null && folderId > 0)
             {
                 predicateFolder = o => o.FolderId == folderId;
             }
 
-            if (levelId != null && levelId > 0)
-            {
-                predicateLevel = o => o.LevelId == levelId;
-            }
-            var temp = db.Document.Where(predicateFolder).Where(predicateLevel).OrderByDescending(t => t.AddDate).Skip(skip ?? 0).Take(take ?? 20).ToList();
+            var temp = db.Document.Where(predicateFolder).OrderByDescending(t => t.AddDate).Skip(skip ?? 0).Take(take ?? 20).ToList();
             var data = temp.Select(t => new { t.Id, t.Title, t.TitleTwo, t.SoundPath, t.Duration, t.Length, t.LengthString, DateString = (t.AddDate == null ? "" : t.AddDate.Value.ToString("yyyy-MM-dd")) });
             return Json(data, JsonRequestBehavior.AllowGet);
         }
@@ -66,7 +64,7 @@ namespace StudyOnline.Controllers
         public ActionResult Folders(int? levelId)
         {
             Func<Folder, bool> predicate = o => true;
-            if (levelId != null)
+            if (levelId != null && levelId > 0)
             {
                 predicate = o => o.LevelId == levelId;
             }
