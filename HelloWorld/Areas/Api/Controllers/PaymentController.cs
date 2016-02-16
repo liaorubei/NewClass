572369fce1e4dd9d55f8f60c7a8ccdad23f86621
liaorubei <liaorubei@outlook.com>
2016-02-16 15:00:31 +0800

    -
    -

--- HelloWorld/Areas/Api/Controllers/PaymentController.cs
+++ HelloWorld/Areas/Api/Controllers/PaymentController.cs
@@ -86,7 +86,7 @@ namespace StudyOnline.Areas.Api.Controllers
         }
 
         [HttpPost]
-        public ActionResult VerifyPayPal(String paymentId, String clientPaymentJson)
+        public ActionResult VerifyPayPal(String paymentId, String rechargeId)
         {
             //OAuthTokenCredential tokenCredential = new OAuthTokenCredential("<CLIENT_ID>", "<CLIENT_SECRET>");
             //string accessToken = tokenCredential.GetAccessToken();
@@ -99,11 +99,19 @@ namespace StudyOnline.Areas.Api.Controllers
             // See [Configuration.cs] to know more about APIContext.
             var apiContext = Configuration.GetAPIContext();
             Payment payment = Payment.Get(apiContext, paymentId);
+			
+			//You should verify that the Payment:
+			//Is approved ("state": "approved").
+			//Contains a Transaction with:
+			//An Amount with total and currency values that match your expectation.
+			//A Sale that is completed (in related_resources, with "state": "completed").
 
             if (payment.state != "approved")
             {
                 return Json(new { code = 201, desc = "交易记录验证不成功", info = payment });
             }
+			
+		Recharge recharge =	entities.Recharge.Find(rechargeId);
 
             //由手机端传过来的信息,如支付总额,货币
             String clientAmount = "";
@@ -121,24 +129,24 @@ namespace StudyOnline.Areas.Api.Controllers
             //验证交易总额
             if (clientAmount != serverAmount)
             {
-
+				return Json(new { code = 201, desc = "交易总额验证不成功", info = payment });
             }
 
             //验证货币类型
             if (clientCurrency != serverCurrentcy)
             {
-
+				return Json(new { code = 201, desc = "货币类型验证不成功", info = payment });
             }
 
             //验证交易状态
             if (saleState != "completed")
             {
-
+				return Json(new { code = 201, desc = "交易状态验证不成功", info = payment });
             }
-
-            //插入交易明细
-
+		//	recharge.state="completed";
+		
             //保存数据
+			
             return Json(payment);
         }
     }
